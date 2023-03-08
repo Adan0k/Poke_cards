4f025d2e05a5907647c24a28f279e495df08b92a
Adan0k <adansilvaoliveira@gmail.com>
2023-03-08 21:28:14 +0000

    fix: a little error from my

--- src/script.js
+++ src/script.js
@@ -3,36 +3,37 @@ document.getElementById("curPkm").addEventListener("change", (e) => {
 });
 
 const card = document.getElementById("card");
+const pkmImage = document.getElementById("pkmImage");
 const getPkmData = (name) => {
     document.getElementById("pkmName").innerHTML = name;
     switch (name) {
         case "charizard":
             card.style.background =
                 "linear-gradient(180deg, rgba(255,0,0,1) 32%, rgba(243,243,243,1) 77%)";
-            document.getElementById("pkmImage").src = "./images/Charizard.png";
+            pkmImage.src = "./images/Charizard.png";
             break;
         case "greninja":
             card.style.background =
                 "linear-gradient(180deg, rgba(0,161,255,1) 40%, rgba(0,0,0,1) 80%)";
-            document.getElementById("pkmImage").src = "./images/Greninja.png";
+            pkmImage.src = "./images/Greninja.png";
             break;
         case "lucario":
             card.style.background =
                 "linear-gradient(0deg, rgba(0,106,168,1) 22%, rgba(133,133,133,1) 38%)";
-            document.getElementById("pkmImage").src = "./images/lucario.png";
+            pkmImage.src = "./images/lucario.png";
             break;
         case "gardevoir":
             card.style.background = "#ff0095";
-            document.getElementById("pkmImage").src = "./images/gardevoir.png";
+            pkmImage.src = "./images/gardevoir.png";
             break;
         case "trevenant":
             card.style.background =
                 "linear-gradient(180deg, rgba(46,255,0,1) 39%, rgba(0,0,0,1) 77%)";
-            document.getElementById("pkmImage").src = "./images/Trevenant.png";
+            pkmImage.src = "./images/Trevenant.png";
             break;
         case "luxray":
             card.style.background = "#fffa00";
-            document.getElementById("pkmImage").src = "./images/luxray.png";
+            pkmImage.src = "./images/luxray.png";
             break;
 
         default:
